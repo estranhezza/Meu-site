007d970f85c7975c07f4d8a9845747bd25da64ac
Crislaine campos <crislainecampos95@gmail.com>
2026-03-30 04:48:30 -0300

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-const botao = document.getElementById('tema-btn');
-botao.addEventListener('click',()=> {
-    document.body.classList.toggle('dark');
-    if(document.body.classList.contains('dark')){
-        botao.innerText="modo claro";
-    } else {
-        botao.innerText="modo escuro";
-    }
-});
